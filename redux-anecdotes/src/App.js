a713fa7ec6d6d9dd344c6cab55ed8e933ff7d089
LIWKaguya <hungle120901@gmail.com>
2021-11-27 05:01:40 +0200

    Exercise 6.7-6.8

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import AnecdoteForm from './components/anecdoteForm'
-import { likeAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
-const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(likeAnecdote(id))
-  }
-
-
-   
+const App = () => {   
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((mine, yours) => yours.votes - mine.votes) 
-      .map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { likeAnecdote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({anecdote}) => {
+    const dispatch = useDispatch()
+    const vote = (id) => {
+        dispatch(likeAnecdote(id))
+    }
+    return (
+        <div>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+    )
+}
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(state => state)
+    return (
+        <>
+        {anecdotes.sort((mine, yours) => yours.votes - mine.votes) 
+        .map(anecdote =>
+            <Anecdote anecdote={anecdote} key={anecdote.id}/>
+            )}
+        </>
+    )
+}
+
+export default AnecdoteList
